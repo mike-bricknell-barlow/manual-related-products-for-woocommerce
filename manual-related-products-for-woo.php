3acca4e1513106f3e7c586b7fa03454256469f35
Mike Bricknell-Barlow <mikeb@elementarydigital.co.uk>
2020-05-24 13:02:17 +0100

    Incrementing version;

--- manual-related-products-for-woo.php
+++ manual-related-products-for-woo.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Manual Related Products for WooCommerce
  * Plugin URI:        
  * Description:       Allows you to choose a manual set of products for display under the Related Products section on WooCommerce single product pages.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 5.0.0
  * Requires PHP:      7.0
  * Author:            Mike Bricknell-Barlow
@@ -13,7 +13,7 @@
  * Text Domain:       manual-related-products-for-woo
 */
 
-define( 'MANUAL_RELATED_PRODUCTS_WOOCOMMERCE_VERSION', '1.0.0' );
+define( 'MANUAL_RELATED_PRODUCTS_WOOCOMMERCE_VERSION', '1.0.1' );
 define( 'MANUAL_RELATED_PRODUCTS_WOOCOMMERCE_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 define( 'MANUAL_RELATED_PRODUCTS_WOOCOMMERCE_PLUGIN_DIR_PATH', dirname( __FILE__ ) );
 
